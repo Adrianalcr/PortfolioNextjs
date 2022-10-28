56d001fd73d7d27e5d3ca778739d0b640642433e
Adriana Lima <adrianalima_webdesigner@outlook.com>
2022-10-28 16:21:22 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,11 +14,9 @@ module.exports = {
 
   compiler: {
     relay: {
-      // This should match relay.config.js
       src: './',
       artifactDirectory: './__generated__',
       language: 'typescript',
     },
   },
-
 }
